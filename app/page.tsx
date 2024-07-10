0c1d77072256f719fad67417d9e158586acd3225
khaymanii <kennethakpo61@gmail.com>
2024-07-10 14:43:06 +0100

    created footer comp

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import Results from "@/components/Results";
 import Feedback from "@/components/Feedback";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <div className="bg-black min-h-screen h-auto">
@@ -12,6 +13,7 @@ export default function Home() {
       <Features />
       <Results />
       <Feedback />
+      <Footer />
     </div>
   );
 }
