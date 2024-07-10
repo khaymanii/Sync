c6439cd347986e9ab02a7b78d65055b9371dcbb1
khaymanii <kennethakpo61@gmail.com>
2024-07-10 16:01:13 +0100

    created footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,6 @@
-"use client";
+/* eslint-disable react/no-string-refs */
 import React from "react";
 import Image from "next/image";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-  Link,
-  Button,
-} from "@nextui-org/react";
 
 export default function Footer() {
   return (
@@ -21,45 +10,6 @@ export default function Footer() {
         <Image src="./images/logo.svg" alt="logo" width={50} height={50} />
       </div>
       <hr className="border-t border-white w-full pb-8" />
-      <Navbar className="bg-black">
-        <NavbarContent className="">
-          <NavbarItem>
-            <Link className="text-white text-xs" href="#">
-              TERMS & CONDITIONS
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
-
-        <NavbarContent className="sm:flex gap-10 " justify="center">
-          <NavbarItem>
-            <Link className="text-white text-xs" href="#">
-              Feature
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link className="text-white text-xs" href="#" aria-current="page">
-              Pricing
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link className="text-white text-xs" href="#">
-              About Us
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link className="text-white text-xs" href="#">
-              Contact
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
-        <NavbarContent justify="end">
-          <NavbarItem>
-            <Link className="text-white text-xs" href="#">
-              PRIVACY POLICY
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
-      </Navbar>
     </div>
   );
 }
